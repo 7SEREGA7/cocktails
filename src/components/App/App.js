c4420f7e2dcdd735e9773aba032edc691a8457b4
Serhiy <serhiytysyak7@gmail.com>
2020-10-20 18:12:21 +0300

    hoc

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
-import Home from '../Pages/Home'
-import Alcoholic from '../Pages/Alcoholic'
-import NonAlcoholic from '../Pages/NonAlcoholic'
 import './App.scss'
 
 export default class App extends Component {
 
   render() {
     return (
-  	  <Router>
-    	  <div className="App">
-    		  <NavBar />
-    	  </div>
-    		<Switch>
-    		  <Route exact path="/" component={Home} />
-    		  <Route path="/alcoholic" component={Alcoholic} />
-    		  <Route path="/non_alcoholic" component={NonAlcoholic} />
-    		</Switch>
-  	  </Router>
-  	)
+     <div className="App">
+      <NavBar />
+     </div>
+    )
   }
 }
